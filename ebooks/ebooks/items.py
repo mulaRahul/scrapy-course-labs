1f6147c2ef4545979076d06c0d5bf9991d212fa2
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-01 16:19:19 +0530

    Update items.py

--- ebooks/ebooks/items.py
+++ ebooks/ebooks/items.py
@@ -1,5 +1,15 @@
-import scrapy
+from scrapy import Item, Field
+from itemloaders.processors import MapCompose, TakeFirst
 
-class EbookItem(scrapy.Item):
-  # name = scrapy.Field()
-  pass
+def get_price(txt):
+    return float(txt.replace('£', '' ))
+
+  
+class EbookItem(Item):
+    title = Field(
+        output_processor=TakeFirst()
+    )
+    price = Field(
+        input_processor=MapCompose(get_price),
+        output_processor=TakeFirst()
+    )
