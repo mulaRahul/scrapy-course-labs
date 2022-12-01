8835ee6d987996894080eb74a9fe2f051d1e5ce3
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-01 18:50:12 +0530

    Update quote.py

--- quotes/quotes/spiders/quote.py
+++ quotes/quotes/spiders/quote.py
@@ -3,7 +3,7 @@ import scrapy
 
 class QuoteSpider(scrapy.Spider):
     name = 'quote'
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/tableful/']
 
     def parse(self, response):
         pass
