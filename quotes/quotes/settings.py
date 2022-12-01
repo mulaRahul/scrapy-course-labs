b6fd949fe0a0a206c079ce22a221f0e63a3238cf
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2022-12-01 20:04:08 +0530

    Update settings.py

--- quotes/quotes/settings.py
+++ quotes/quotes/settings.py
@@ -4,9 +4,3 @@ SPIDER_MODULES = ['quotes.spiders']
 NEWSPIDER_MODULE = 'quotes.spiders'
 
 ROBOTSTXT_OBEY = True
-
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-}
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
